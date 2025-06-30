248099fe4dd665c9570d63edc95dfa1489234429
SalamJean <salamjeanlouis8@gmail.com>
2025-06-30 16:08:04 +0000

    error PDF

--- app/Http/Controllers/AbonnementController.php
+++ app/Http/Controllers/AbonnementController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Abonnement;
 use App\Models\Visite;
-use Barryvdh\DomPDF\Facade\PDF;
+use PDF;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
