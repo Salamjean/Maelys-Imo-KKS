01eb2f9ba162f3a2f94483b18783f1a639cffac5
unknown <salamjeanlouis3@gmail.com>
2025-10-01 10:39:44 +0000

    fix: return url

--- resources/views/locataire/paiements/success.blade.php
+++ resources/views/locataire/paiements/success.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Paiement Réussi - Confirmation</title>
+     <link rel="icon" type="image/png" href="{{ asset('assets/images/mae-imo.png') }}">
     <style>
         * {
             margin: 0;
